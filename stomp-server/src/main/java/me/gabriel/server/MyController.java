0ab4b30e61611ca2f58fbaf4803990f13ff5374b
Gabriel Honda <gabrielhondacba@gmail.com>
2021-07-24 17:27:55 -0400

    feat: adiciona comunicação websocket pelo spring boot

---
+++ stomp-server/src/main/java/me/gabriel/server/MyController.java
@@ -0,0 +1,28 @@
+package me.gabriel.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author daohn
+ * @since 24/07/2021
+ */
+@Controller
+public class MyController {
+
+  private final SimpMessagingTemplate template;
+
+  @Autowired
+  public MyController(SimpMessagingTemplate template) {
+    this.template = template;
+  }
+
+  @MessageMapping("/sensors")
+  public void handleReading(@Payload String inbound) {
+    System.out.println(inbound);
+    this.template.convertAndSend("/commands/sample", inbound);
+  }
+}
---
+++ stomp-server/src/main/java/me/gabriel/server/MyRestController.java
@@ -0,0 +1,29 @@
+package me.gabriel.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author daohn
+ * @since 24/07/2021
+ */
+@RestController
+public class MyRestController {
+
+  private final SimpMessagingTemplate template;
+
+  @Autowired
+  public MyRestController(SimpMessagingTemplate template) {
+    this.template = template;
+  }
+
+  @GetMapping("/enviar")
+  public void send() {
+    System.out.println("Enviando mensagem");
+    this.template.convertAndSend("/commands/sample", "Olá esp");
+
+  }
+
+}
