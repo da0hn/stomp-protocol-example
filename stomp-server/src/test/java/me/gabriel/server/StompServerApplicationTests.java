c850293c90b8cba64adbfc2ce7c8daa994d9c928
Gabriel Honda <gabrielhondacba@gmail.com>
2021-07-24 15:45:06 -0400

    commit inicial

---
+++ stomp-server/src/test/java/me/gabriel/server/StompServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabriel.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
